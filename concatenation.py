a4a8ccf90393b97e9c95447a8926f553d559d3c5
Kelvin-Wanjiku <98302705+Kelvin-Wanjiku@users.noreply.github.com>
2022-02-20 21:00:54 -0800

    Add files via upload

---
+++ concatenation.py
@@ -0,0 +1,4 @@
+phrase="python"
+print(phrase+ "is a good language to learn".upper())
+print("length of sentence is:")
+print(len(phrase))
---
+++ while with break statement.py
@@ -0,0 +1,7 @@
+i=1
+while i<9:
+    print(i)
+    i+=1
+    if i==3:
+        break
+        i+=1
