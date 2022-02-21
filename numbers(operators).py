be09d86af99286af272a56b245473f8872e96933
Kelvin-Wanjiku <98302705+Kelvin-Wanjiku@users.noreply.github.com>
2022-02-20 22:21:52 -0800

    Add files via upload

---
+++ numbers(operators).py
@@ -0,0 +1,20 @@
+from math import*
+
+print(pow(4,2))
+print(round(3.4))
+
+#order of operations
+
+print(5*3+4)
+print(5*(3+4))
+
+#Absolute of a number
+a=-5
+print(abs(a))
+
+#printing largest and smallest number
+print(max(5,7))
+print(min(2,7))
+
+#finding the square root of a number
+print(sqrt(36))
