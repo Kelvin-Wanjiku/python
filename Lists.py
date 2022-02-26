535badacc2b3182394b220057900156ebac2bfb1
kelvin-wanjiku <kelvinwanjiku249@gmail.com>
2022-02-26 09:54:49 -0800

    https://github.com/Kelvin-Wanjiku/python

---
+++ Lists.py
@@ -0,0 +1,17 @@
+friends=["Judy", "Mike", "Willy"]
+print(friends)
+
+#updating the list
+friends[1]="Vinny"
+print(friends)
+
+# extending the list by attaching another list
+other_friends=["Mike", "Steshiah"]
+friends.extend(other_friends)
+print(friends)
+
+#inserting an element
+friends.insert(1,"John")
+print(friends)
+
+
---
+++ if else statement.py
@@ -0,0 +1,14 @@
+#if statement
+num=2
+if num>0:
+    print(num,"is a positive number.")
+
+#if else statement
+
+a=int(input("enter a number:"))
+b=int(input("enter a second number:"))
+
+if a>b:
+    print(a,"is greater than", +b)
+else:
+    print(a,"is less than " ,+b)
