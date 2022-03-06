49a4b1f926d1122236660fff1a762f1c7cdb16b0
Kelvin-Wanjiku <98302705+Kelvin-Wanjiku@users.noreply.github.com>
2022-03-06 09:33:56 +0300

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,15 @@
+fruits=["apple", "banana", "mango"]
+for x in fruits:
+    print(x)
+
+#for loop with break statement
+print("\n")
+for x in fruits:
+    print(x)
+    if x=="banana":
+        break
+
+#for loop with range function
+print("\n for loop with a range function:")
+for x in range(7):
+    print(x)
